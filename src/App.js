d6ef425de4a353f433ceef1a16ae79d8a8f50e62
Spandana B Gowda <spandanabgowda0303@gmail.com>
2023-11-20 02:37:07 -0800

    firstss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from 'react'
-import Navigation from './Navigation'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './Components/Home'
-import Contact from './Components/Contact'
-import TodoList from './Components/TodoList'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <div>
-      <Navigation/>
-      </div>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/todolist' element={<TodoList/>}/>
-      </Routes>
-      </Router>
-      </div>
+    <div>App</div>
   )
 }
 
